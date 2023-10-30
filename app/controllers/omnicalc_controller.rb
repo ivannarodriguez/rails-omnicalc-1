577782e6d20280304afcfa43226fb6dd14775639
Ivanna <44304361+ivannarodriguez@users.noreply.github.com>
2023-10-30 02:58:37 +0000

    close?

---
+++ app/controllers/omnicalc_controller.rb
@@ -0,0 +1,49 @@
+class OmnicalcController < ApplicationController
+  def newsquare
+    render({:template => "new_square_calc"})
+  end
+
+  def squareresult
+    @user_num = params[:users_number]
+    @square_user_num = params[:users_number].to_f**2
+    render({:template => "square_results"})
+  end
+
+  def newsquareroot
+    render({:template => "new_square_root_calc"})
+  end
+
+  def squarerootresult
+    @user_num = params[:users_number]
+    @square_root_user_num = Math.sqrt(@user_num.to_f)
+    render({:template => "square_root_results"})
+  end
+
+  def newpayment
+    render({:template => "new_payment")
+  end
+
+  def paymentresult
+    @apr = params[:user_apr].to_f
+    @years = params[:user_years].to_f
+    @principal = params[:user_pv].to_f.round(2)
+  
+    monthly_interest_rate = (@apr / 12) / 100
+    total_payments = @years * 12
+    @payment = (@principal * monthly_interest_rate * (1 + monthly_interest_rate)**total_payments) / ((1 + monthly_interest_rate)**total_payments - 1)
+    @payment = @payment.round(2)
+   
+    render({:template => "payment_results"})
+  end
+
+  def newrandom
+    render({:template => "new_random"})
+  end
+
+  def randomresult
+    @min = params[:user_min].to_f
+    @max = params[:user_max].to_f
+    @random_number= rand(@min..@max)
+    render({:template => "random_result"})
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  
+  get("/", {:controller => "omnicalc", :action => "backhome"})
+  get("/square/new", {:controller => "omnicalc", :action => "newsquare"})
+  get("/square/results", {:controller => "omnicalc", :action => "squareresult"})
+  get("/square_root/new", {:controller => "omnicalc", :action => "newsquareroot"})
+  get("/square_root/results", {:controller => "omnicalc", :action => "squarerootresult"})
+  get("/payment/new", {:controller => "dice", :action => "newpayment"})
+  get("/payment/results", {:controller => "dice", :action => "paymentresult"})
+  get("/random/new", {:controller => "dice", :action => "newrandom"})
+  get("/random/results", {:controller => "dice", :action => "randomresult"})
 end
